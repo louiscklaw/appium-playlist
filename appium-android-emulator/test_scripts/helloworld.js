e342da186ebc4e22d5fc9e61d63799543c15e3a1
louiscklaw <louiscklaw@gmail.com>
2020-08-09 01:54:52 +0800

    adding merger files,

--- appium-android-emulator/test_scripts/helloworld.js
+++ appium-android-emulator/test_scripts/helloworld.js
@@ -3,11 +3,12 @@ const wdio = require("webdriverio");
 
 const opts = {
   path: '/wd/hub',
-  port: 4444,
+  port: 4723,
   capabilities: {
     platformName: "Android",
     platformVersion: "7.1.1",
     app: "/root/apk_pool/ApiDemos-debug.apk",
+    fullReset: true
     // deviceName: "nexus_5_7.1.1",
     // appPackage: "io.appium.android.apis",
     // appActivity: ".view.TextFields",
