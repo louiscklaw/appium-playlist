0b12961f4c041dc9ba908af4833a0e58e695cb03
louiscklaw <louiscklaw@gmail.com>
2020-11-12 20:00:23 +0800

    init commit and test,

---
+++ appium-android-10-tryout/test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ex
+
+trap 'catch' ERR EXIT KILL
+catch() {
+  echo 'exit script, shutdown docker...'
+  scripts/down_docker.sh
+}
+
+scripts/down_docker.sh | true
+scripts/start_docker.sh
+sleep 90
+
+echo 'testing python appium client'
+pushd tests/python
+  pipenv sync
+  pipenv run python3 ./helloworld.py
+
+popd
+
+
+echo 'testing javascipt appium client'
+pushd tests/javascript
+  yarn
+  node ./helloworld.js
+popd
