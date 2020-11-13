6aec4e58cdc691d07b556bddc750dd62359d247e
louiscklaw <louiscklaw@gmail.com>
2020-11-13 22:04:37 +0800

    update and test,

--- appium-android-9-tryout/run.sh
+++ appium-android-9-tryout/run.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+trap 'catch' ERR EXIT KILL
+catch() {
+  echo 'exit script, shutdown docker...'
+  scripts/down_docker.sh
+}
 
+scripts/down_docker.sh | true
 scripts/start_docker.sh
+sleep 90
+
 scripts/run_test.sh
