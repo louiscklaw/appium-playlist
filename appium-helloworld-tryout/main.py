a7e64d2141b22d96db0505e5ea1a46d949715716
louiscklaw <louiscklaw@gmail.com>
2020-12-01 19:59:17 +0800

    update and test,

--- appium-helloworld-tryout/main.py
+++ appium-helloworld-tryout/main.py
@@ -8,6 +8,7 @@ desired_caps['platformName'] = 'Android'
 
 # reference to server side
 desired_caps['app'] = 'apk_pool/ApiDemos-debug.apk'
+desired_caps['uiautomator2ServerLaunchTimeout'] = 60*1000
 
 # This will launch your Android application.
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
