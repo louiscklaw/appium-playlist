25f06e6f343866ed95f1f83287a60664dd3cb453
louiscklaw <louiscklaw@gmail.com>
2020-12-05 23:09:23 +0800

    update and test,

--- appium-android-10-tryout/build_phone.sh
+++ appium-android-10-tryout/build_phone.sh
@@ -3,35 +3,13 @@
 
 set -ex
 
-ANDROID_NAMES=(\
-  android_a \
-  android_b \
-  android_c \
-  android_d \
-  android_e \
-  )
-ADB_ANDROIDS=(\
-  'adb -s emulator-5554' \
-  'adb -s emulator-5556' \
-  'adb -s emulator-5558' \
-  'adb -s emulator-5560' \
-  'adb -s emulator-5562' \
-  )
-
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-30;google_apis;x86_64'
 
 $ANDROID_HOME/tools/bin/sdkmanager --list
 $ANDROID_HOME/tools/bin/avdmanager list
 
-i=0
-echo ${ADB_ANDROIDS[$i]}
-echo ${ANDROID_NAMES[$i]}
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd --device "pixel" -n android_a -k 'system-images;android-30;google_apis;x86_64' --force --sdcard 512M
 
-ACTIVE_ANDROID_NAME=${ANDROID_NAMES[$i]}
-ACTIVE_ADB_ANDROID=${ADB_ANDROIDS[$i]}
+nohup $ANDROID_HOME/emulator/emulator -avd android_a -no-snapshot > /dev/null 2>&1 &
 
-echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd --device "pixel" -n $ACTIVE_ANDROID_NAME -k 'system-images;android-30;google_apis;x86_64' --force --sdcard 512M
-
-nohup $ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot > /dev/null 2>&1 &
-
-$ANDROID_HOME/platform-tools/$ACTIVE_ADB_ANDROID wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+$ANDROID_HOME/platform-tools/adb -s emulator-5554 wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
