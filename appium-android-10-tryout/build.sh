93600c8c91e35cdde60e8aacdf1ee58b68f9e6c7
louiscklaw <louiscklaw@gmail.com>
2020-12-05 22:19:31 +0800

    update and test,

--- appium-android-10-tryout/build.sh
+++ appium-android-10-tryout/build.sh
@@ -18,7 +18,7 @@ ADB_ANDROIDS=(\
   'adb -s emulator-5562' \
   )
 
-echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;${{ matrix.ansdroid_api_ver }};google_apis;x86_64'
+echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-30;google_apis;x86_64'
 
 $ANDROID_HOME/tools/bin/sdkmanager --list
 $ANDROID_HOME/tools/bin/avdmanager list
