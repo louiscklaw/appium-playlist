105481fca9a5546dea042a7a19ca7d81d4a4f9fe
runner <runner@Mac-1607176789517.local>
2020-12-05 16:47:25 +0000

    update,

--- appium-android-10-tryout/build_tablet.sh
+++ appium-android-10-tryout/build_tablet.sh
@@ -12,12 +12,13 @@ echo "create a tablet device start"
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd --device "Nexus 10" -n $ACTIVE_ANDROID_NAME -k 'system-images;android-30;google_apis;x86_64' --force --sdcard 512M
 
 # nohup $ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot > /dev/null 2>&1 &
-$ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot &
+nohup $ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot &
 
 $ANDROID_HOME/platform-tools/$ACTIVE_ADB_ANDROID wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 echo "create a tablet device end"
 
 echo $ANDROID_HOME/emulator/emulator -list-avds
+
 
 while [ "$($ACTIVE_ADB_ANDROID shell getprop sys.boot_completed | tr -d '\r')" != "1" ]; do
   echo "Still waiting for boot.."
