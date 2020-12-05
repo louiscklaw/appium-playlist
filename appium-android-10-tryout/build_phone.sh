f4e47c6fadf699e4d4eba0000dd18bbb98ca6ac3
louiscklaw <louiscklaw@gmail.com>
2020-12-05 23:32:47 +0800

    update and test,

--- appium-android-10-tryout/build_phone.sh
+++ appium-android-10-tryout/build_phone.sh
@@ -8,8 +8,9 @@ echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-3
 $ANDROID_HOME/tools/bin/sdkmanager --list
 $ANDROID_HOME/tools/bin/avdmanager list
 
-echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd --device "pixel" -n android_a -k 'system-images;android-30;google_apis;x86_64' --force --sdcard 512M
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd --device "pixel" -n android_c -k 'system-images;android-30;google_apis;x86_64' --force --sdcard 512M
 
-nohup $ANDROID_HOME/emulator/emulator -avd android_a -no-snapshot > /dev/null 2>&1 &
+$ANDROID_HOME/emulator/emulator -avd android_c -no-snapshot
+# nohup $ANDROID_HOME/emulator/emulator -avd android_c -no-snapshot > /dev/null 2>&1 &
 
 $ANDROID_HOME/platform-tools/adb -s emulator-5554 wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
