f700d7cfa5654a2f8d1d39264b4977abd13e3355
louiscklaw <louiscklaw@gmail.com>
2020-12-05 22:38:29 +0800

    update and test,

---
+++ appium-android-10-tryout/build_appium.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -ex
+
+npm install appium@1.15.0
--- appium-android-10-tryout/build_tablet.sh
+++ appium-android-10-tryout/build_tablet.sh
@@ -13,3 +13,9 @@ $ANDROID_HOME/platform-tools/$ACTIVE_ADB_ANDROID wait-for-device shell 'while [[
 echo "create a tablet device end"
 
 echo $ANDROID_HOME/emulator/emulator -list-avds
+
+# TODO: activate me
+# while [ "$($ACTIVE_ADB_ANDROID shell getprop sys.boot_completed | tr -d '\r')" != "1" ]; do
+#   echo "Still waiting for boot.."
+#   sleep 1
+# done
