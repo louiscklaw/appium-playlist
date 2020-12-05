5d50a465beb7d10a2560cfe2fd5117b51b93eff2
runner <runner@Mac-1607176789517.local>
2020-12-05 17:11:15 +0000

    update working commit,

--- appium-android-10-tryout/build_tablet.sh
+++ appium-android-10-tryout/build_tablet.sh
@@ -3,7 +3,7 @@
 set -ex
 
 ACTIVE_ANDROID_NAME=tablet_a
-ACTIVE_ADB_ANDROID='adb -s emulator-5562'
+ACTIVE_ADB_ANDROID='adb -s emulator-5554'
 
 # adb -s emulator-5562 emu kill
 
@@ -12,18 +12,18 @@ echo "create a tablet device start"
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd --device "Nexus 10" -n $ACTIVE_ANDROID_NAME -k 'system-images;android-30;google_apis;x86_64' --force --sdcard 512M
 
 # nohup $ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot > /dev/null 2>&1 &
-nohup $ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot &
+$ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot &
 
 $ANDROID_HOME/platform-tools/$ACTIVE_ADB_ANDROID wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 echo "create a tablet device end"
 
-echo $ANDROID_HOME/emulator/emulator -list-avds
+#echo $ANDROID_HOME/emulator/emulator -list-avds
 
 
-while [ "$($ACTIVE_ADB_ANDROID shell getprop sys.boot_completed | tr -d '\r')" != "1" ]; do
-  echo "Still waiting for boot.."
-  sleep 1
-done
+#while [ "$($ACTIVE_ADB_ANDROID shell getprop sys.boot_completed | tr -d '\r')" != "1" ]; do
+#  echo "Still waiting for boot.."
+#  sleep 1
+#done
 
 echo 'boot tablet done'
 
