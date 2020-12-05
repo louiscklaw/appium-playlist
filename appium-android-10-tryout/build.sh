9fa7aac0a9c5fa895f8b7dd9ef48466554d978f3
louiscklaw <louiscklaw@gmail.com>
2020-12-05 22:22:52 +0800

    update and test,

--- appium-android-10-tryout/build.sh
+++ appium-android-10-tryout/build.sh
@@ -23,18 +23,15 @@ echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-3
 $ANDROID_HOME/tools/bin/sdkmanager --list
 $ANDROID_HOME/tools/bin/avdmanager list
 
+i=0
+echo ${ADB_ANDROIDS[$i]}
+echo ${ANDROID_NAMES[$i]}
 
-for i in {0..3}
-do
-  echo ${ADB_ANDROIDS[$i]}
-  echo ${ANDROID_NAMES[$i]}
+ACTIVE_ANDROID_NAME=${ANDROID_NAMES[$i]}
+ACTIVE_ADB_ANDROID=${ADB_ANDROIDS[$i]}
 
-  ACTIVE_ANDROID_NAME=${ANDROID_NAMES[$i]}
-  ACTIVE_ADB_ANDROID=${ADB_ANDROIDS[$i]}
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd --device "pixel" -n $ACTIVE_ANDROID_NAME -k 'system-images;android-30;google_apis;x86_64' --force --sdcard 512M
 
-  echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd --device "pixel" -n $ACTIVE_ANDROID_NAME -k 'system-images;android-30;google_apis;x86_64' --force --sdcard 512M
+nohup $ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot > /dev/null 2>&1 &
 
-  nohup $ANDROID_HOME/emulator/emulator -avd $ACTIVE_ANDROID_NAME -no-snapshot > /dev/null 2>&1 &
-
-  $ANDROID_HOME/platform-tools/$ACTIVE_ADB_ANDROID wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
-done
+$ANDROID_HOME/platform-tools/$ACTIVE_ADB_ANDROID wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
