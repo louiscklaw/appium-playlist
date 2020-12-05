51060a3d57251c8772fb5d84d00eca80f2fee017
louiscklaw <louiscklaw@gmail.com>
2020-12-05 22:16:16 +0800

    update and test,

--- appium-android-10-tryout/build.sh
+++ appium-android-10-tryout/build.sh
@@ -4,3 +4,6 @@
 set -ex
 
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;${{ matrix.ansdroid_api_ver }};google_apis;x86_64'
+
+$ANDROID_HOME/tools/bin/sdkmanager --list
+$ANDROID_HOME/tools/bin/avdmanager list
