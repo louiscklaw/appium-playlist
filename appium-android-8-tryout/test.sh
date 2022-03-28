8f0e04f88f55d7ed81d119c0ce2899a2e3245e9a
louiscklaw <logickee@gmail.com>
2022-03-28 21:52:25 +0800

    update pipenv python,

--- appium-android-8-tryout/test.sh
+++ appium-android-8-tryout/test.sh
@@ -2,12 +2,25 @@
 
 set -ex
 
-# echo 'testing python appium client'
-# pushd tests/python
-#   pipenv sync
-#   pipenv run python3 ./helloworld.py
+pipenv -v
+python3 -V
 
-# popd
+trap 'catch' ERR EXIT KILL
+catch() {
+  echo 'exit script, shutdown docker...'
+  scripts/down_docker.sh
+}
+
+scripts/down_docker.sh | true
+scripts/start_docker.sh
+sleep 90
+
+echo 'testing python appium client'
+pushd tests/python
+  pipenv sync
+  pipenv run python3 ./helloworld.py
+
+popd
 
 echo 'testing javascipt appium client'
 pushd tests/javascript
