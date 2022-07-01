e0ae0d611a251ed9afa2c3db5fb6ce10ba71333c
louiscklaw <louiscklaw@gmail.com>
2022-07-01 19:05:32 +0800

    update appium-helloworld-tryout,

---
+++ appium-helloworld-tryout/py/main.py
@@ -0,0 +1,20 @@
+from appium import webdriver
+
+print('hello appium')
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+# desired_caps['appPackage'] = 'net.openwritings.xmtl'
+# desired_caps['appActivity'] = '.MainActivity'
+# desired_caps['deviceName']='nexus_5_7.1.1'
+
+# reference to server side
+desired_caps['app'] = 'apk_pool/ApiDemos-debug.apk'
+desired_caps['uiautomator2ServerLaunchTimeout'] = 60*1000
+
+# This will launch your Android application.
+driver = webdriver.Remote('http://192.168.10.21:4444/wd/hub', desired_caps)
+
+driver.save_screenshot('screens/helloworld.png')
+
+print('done')
