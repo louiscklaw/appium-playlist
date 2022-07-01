938043e2b13ce3c4abfe775d5924c0d6ebd35485
louiscklaw <louiscklaw@gmail.com>
2022-07-01 19:22:40 +0800

    update appium-helloworld-tryout,

--- appium-helloworld-tryout/py/main.py
+++ appium-helloworld-tryout/py/main.py
@@ -9,7 +9,7 @@ desired_caps['platformName'] = 'Android'
 # desired_caps['deviceName']='nexus_5_7.1.1'
 
 # reference to server side
-desired_caps['app'] = 'apk_pool/ApiDemos-debug.apk'
+desired_caps['app'] = 'apk_pool/aut.apk'
 desired_caps['uiautomator2ServerLaunchTimeout'] = 60*1000
 
 # This will launch your Android application.
