d64167e5ebacd27f085a4194baa6df94d6993861
louiscklaw <louiscklaw@gmail.com>
2022-07-02 20:32:07 +0800

    update appium-helloworld-tryout,

--- appium-helloworld-tryout/js/index.js
+++ appium-helloworld-tryout/js/index.js
@@ -1,1 +1,20 @@
-console.log("helloworld");
+const wdio = require("webdriverio");
+
+const opts = {
+  hostname: "http://192.168.10.21",
+  path: "/wd/hub",
+  port: 4444,
+  capabilities: {
+    platformName: "Android",
+    app: "apk_pool/ApiDemos-debug.apk",
+    // uiautomator2ServerLaunchTimeout: 60 * 1000,
+  },
+};
+
+async function main() {
+  const client = await wdio.remote(opts);
+
+  await client.deleteSession();
+}
+
+main();
--- appium-helloworld-tryout/py/main.py
+++ appium-helloworld-tryout/py/main.py
@@ -1,3 +1,5 @@
+# https://github.com/appium/appium/tree/master/sample-code
+
 from appium import webdriver
 
 print('hello appium')
@@ -9,8 +11,8 @@ desired_caps['platformName'] = 'Android'
 # desired_caps['deviceName']='nexus_5_7.1.1'
 
 # reference to server side
-desired_caps['app'] = 'apk_pool/aut.apk'
-desired_caps['uiautomator2ServerLaunchTimeout'] = 60*1000
+desired_caps['app'] = 'apk_pool/ApiDemos-debug.apk'
+# desired_caps['uiautomator2ServerLaunchTimeout'] = 60*1000
 
 # This will launch your Android application.
 driver = webdriver.Remote('http://192.168.10.21:4444/wd/hub', desired_caps)
